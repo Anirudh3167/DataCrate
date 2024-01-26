4eb8cb04a8358ed8dead4054d46c9f5e54cbca62
Anirudh3167 <77240291+Anirudh3167@users.noreply.github.com>
2024-01-26 23:30:39 +0530

    minor changes

--- DataCrate/FileStore/views.py
+++ DataCrate/FileStore/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def Home(request):
-    return render(request,'FileStore/FileStoreHome.html')
+    return render(request,"FileStore/FileStoreHome.html")
